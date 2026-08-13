7a4dd851dc48310f5dc7f850a786dfb75270fb66
freecree <bohdan.sidorov@a-bank.com.ua>
2026-08-13 14:01:51 +0300

    Correct naimes

--- homework-7.js
+++ homework-7.js
@@ -1,26 +1,26 @@
 // homework №7.3
-function choweTemperatureCyty(cyty, temperature) {
-  console.log(`Сейчас в ${cyty} температура ${temperature} градусов по Цельсию`)  
+function showTemperatureCity(city, temperature) {
+  console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`)  
 }
-choweTemperatureCyty('Борова', '20')
+showTemperatureCity('Борова', '20')
 
-const choweCytyTemperature = (cyty, temperature) => {
-  return `Сейчас в ${cyty} температура ${temperature} градусов по Цельсию`
+const chowCityTemperature = (city, temperature) => {
+  return `Сейчас в ${city} температура ${temperature} градусов по Цельсию`
 }
-console.log(choweCytyTemperature('Борова', '23'))
+console.log(chowCityTemperature('Борова', '23'))
 
 // homework №7.4
 const LIGHT_SPEED = 299792458;
-function comparedeLight(light) {
-  if (light < LIGHT_SPEED) {
+function compareLightSpeed(speed) {
+  if (speed < LIGHT_SPEED) {
     console.log('Субсветовая скорость')
-  } else if (light === LIGHT_SPEED) {
+  } else if (speed === LIGHT_SPEED) {
     console.log('Скорость света')  
   } else {
     console.log('Сверхсветовая скорость')
   }
 }
-comparedeLight(7949078)
+compareLightSpeed(7949078)
 
 // homework №7.5
 const product = "Пылесос";
@@ -42,5 +42,5 @@ const addThreeNumbers = (num1, num2, num3) => {
 addThreeNumbers(20, 30, 15)
 
 // homework №7.7
-let myAddress = 'Боровая, ул.Черника';
+let myAddress = 'Боровая, ул.Поповича, 5-а';
 console.log(myAddress)
