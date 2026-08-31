1699afcd665b4d3b089d3310cc36625b21c63319
freecree <bohdan.sidorov@a-bank.com.ua>
2026-08-31 16:26:38 +0300

    error correction 1

--- homework-8.js
+++ homework-8.js
@@ -7,8 +7,8 @@ const personData = {
   cityResidence: 'Борова',
   cityBirth: 'Киев',
   education: 'Высшее'
-}
-console.log(Object.entries(personData))
+};
+console.log(Object.entries(personData));
 
 // Homework 7.4
 const vehicleDescription = {
@@ -17,9 +17,9 @@ const vehicleDescription = {
   year: 2020,
   color: 'Black',
   carTransmission: 'Automatic'
-}
-vehicleDescription.owner = personData
-console.log(vehicleDescription)
+};
+vehicleDescription.owner = personData;
+console.log(vehicleDescription);
 
 // Homework 7.5
 const addMaxSpeed = (vehicleDescription, maxSpeed) => {
@@ -29,9 +29,9 @@ const addMaxSpeed = (vehicleDescription, maxSpeed) => {
   } else {
     vehicleDescription.maxSpeed = maxSpeed
   }
-}
-addMaxSpeed(vehicleDescription, 250)
-console.log(vehicleDescription)
+};
+addMaxSpeed(vehicleDescription, 250);
+console.log(vehicleDescription);
 
 // Homework 7.5 мethod 2
 const addMaxSpeed1= (vehicleDescription, maxSpeed) => {
@@ -39,64 +39,76 @@ const addMaxSpeed1= (vehicleDescription, maxSpeed) => {
     vehicleDescription.maxSpeed = maxSpeed;
   }
 };
-addMaxSpeed1(vehicleDescription, 250)
-console.log(vehicleDescription)
+addMaxSpeed1(vehicleDescription, 250);
+console.log(vehicleDescription);
 
 // Homework 7.6
 const showObjectValues = (object, key) => {
   console.log(object[key])
-}
-showObjectValues(vehicleDescription, 'brand')
+};
+showObjectValues(vehicleDescription, 'brand');
 
 // Homework 7.7
 const productList = [ 'Молоко', 'Хлеб', 'Сыр', 'Яйца', 'Масло' ]
-console.log(productList)
+console.log(productList);
 
 // Homework 7.8
 const dogBreedList = [
-  { breed: 'Бордер колли',
+  { 
+    breed: 'Бордер колли',
     country: 'Великобритания',
     groupFci: 1,
     intendedUse: 'Пастушья собака',
-    size: 'Средняя'},
-  { breed: 'Американский стаффордширский терьер',
+    size: 'Средняя'
+  },
+  { 
+    breed: 'Американский стаффордширский терьер',
     country: 'США',
     groupFci: 3,
     intendedUse: 'Служебная собака',
-    size: 'Средняя'},
-  { breed: 'Аляскинский маламут',
+    size: 'Средняя'
+  },
+  { 
+    breed: 'Аляскинский маламут',
     country: 'США',
     groupFci: 5,
     intendedUse: 'Ездовая собака',
-    size: 'Крупная'}  
-]
+    size: 'Крупная'
+  }  
+];
 const akita = {
   breed: 'Американская акита',
   country: 'США',
   groupFci: 5,
   intendedUse: 'Охотничья собака',
   size: 'Крупная'
-}
-dogBreedList.push(akita)
-console.log(dogBreedList)
+};
+dogBreedList.push(akita);
+console.log(dogBreedList);
 
 // Homework 7.9
 const serviceDogList = [
-  { breed: 'Немецкая овчарка',
+  { 
+    breed: 'Немецкая овчарка',
     country: 'Германия',
     groupFci: 1,
     intendedUse: 'Служебная собака',
-    size: 'Крупная'},
-  { breed: 'Русский черный терьер',
+    size: 'Крупная'
+  },
+  { 
+    breed: 'Русский черный терьер',
     country: 'Россия',
     groupFci: 2,
     intendedUse: 'Служебная собака',
-    size: 'Крупная'},
-  { breed: 'Бельгийская овчарка',
+    size: 'Крупная'
+  },
+  { 
+    breed: 'Бельгийская овчарка',
     country: 'Бельгия',
     groupFci: 1,
     intendedUse: 'Служебная собака',
-    size: 'Крупная'}  
+    size: 'Крупная'
+  }  
 ]
 
 const newdogBreedList = [ ...dogBreedList, ...serviceDogList ]
